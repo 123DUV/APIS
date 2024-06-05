12c7359a64dff82d79c3b424e5d000059db1b6f9
Duvan b <mendezban416@gmail.com>
2024-06-05 08:13:08 -0500

    falta paginador y ya

--- pokeapijava/src/principal/crud.java
+++ pokeapijava/src/principal/crud.java
@@ -20,9 +20,8 @@ public class crud extends javax.swing.JFrame {
 
     JButton[] botones;
     JsonArray lista;
-    int enPagina = 20;
-    int paginaActual = 1;
-    int totalPaginas;
+    String siguiente;
+    String anterior;
     DefaultTableModel modelo;
 
     public crud() {
@@ -36,7 +35,7 @@ public class crud extends javax.swing.JFrame {
         setVisible(true);
         setLocationRelativeTo(null);
         cargarBotones();
-        pasar();
+       pack();
 
     }
 
@@ -64,13 +63,15 @@ public class crud extends javax.swing.JFrame {
             e.printStackTrace();
         }
     }
-    int offset = (paginaActual - 1) * 20;
+    
 
     public void cargarBotones() {
         panelBotones.removeAll();
         ConsumoAPI consumo = new ConsumoAPI();
-        String respuesta = consumo.consumoGET("https://pokeapi.co/api/v2/pokemon?=offset" + offset + "&limit=20");
+        String respuesta = consumo.consumoGET("https://pokeapi.co/api/v2/pokemon");
         JsonObject list = JsonParser.parseString(respuesta).getAsJsonObject();
+        siguiente = list.get("next").getAsString();
+        
         lista = list.getAsJsonArray("results");
         System.out.println(list);
 
@@ -104,48 +105,7 @@ public class crud extends javax.swing.JFrame {
 
     }
 
-    public void pasar() {
-        panelPasar.removeAll();
-
-        panelPasar.setLayout(new BoxLayout(panelPasar, BoxLayout.X_AXIS));
-        JButton anterior = new JButton("<");
-        anterior.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (paginaActual > 1) {
-                    paginaActual--;
-                    cargarBotones();
-
-                }
-            }
-        });
-        totalPaginas = (int) Math.ceil((double) lista.size() / enPagina);
-        panelPasar.add(anterior);
-        for (int i = 1; i < totalPaginas; i++) {
-            JButton actual = new JButton(String.valueOf(i));
-
-            actual.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    paginaActual = Integer.parseInt(e.getActionCommand());
-                    cargarBotones();
-
-                }
-            });
-            panelPasar.add(actual);
-        }
-        JButton siguiente = new JButton(">");
-        anterior.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (paginaActual < totalPaginas) {
-                    paginaActual++;
-                    cargarBotones();
-
-                }
-            }
-        });
-        panelPasar.add(siguiente);
-        panelPasar.revalidate();
-        panelPasar.repaint();
-    }
+    
 
     public void mostrarInfo(String url) {
         System.out.println(url);
@@ -181,6 +141,8 @@ public class crud extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
         icono = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -206,7 +168,7 @@ public class crud extends javax.swing.JFrame {
         panelPasar.setLayout(panelPasarLayout);
         panelPasarLayout.setHorizontalGroup(
             panelPasarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 513, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         panelPasarLayout.setVerticalGroup(
             panelPasarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -226,18 +188,38 @@ public class crud extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tabla);
 
+        jButton1.setText(">");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("<");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(panelPasar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(78, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(63, 63, 63))
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(28, 28, 28)
-                .addComponent(icono, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(icono, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(10, 10, 10)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 6, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -245,8 +227,16 @@ public class crud extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(14, 14, 14)
                 .addComponent(icono, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(62, 62, 62)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(74, 74, 74)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(62, 62, 62)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(75, 75, 75)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 137, Short.MAX_VALUE)
                 .addComponent(panelPasar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -270,9 +260,98 @@ public class crud extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       panelBotones.removeAll();
+       ConsumoAPI consumo = new ConsumoAPI();
+        String respuesta = consumo.consumoGET(siguiente);
+        JsonObject list = JsonParser.parseString(respuesta).getAsJsonObject();
+        siguiente = list.get("next").getAsString();
+        anterior = list.get("previous").getAsString();
+        System.out.println(siguiente);
+        
+        lista = list.getAsJsonArray("results");
+        System.out.println(list);
+
+        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
+        botones = new JButton[lista.size()];
+        for (int i = 0; i < lista.size(); i++) {
+            JsonObject pokemon = lista.get(i).getAsJsonObject();
+            String nombre = pokemon.get("name").getAsString();
+            String url = pokemon.get("url").getAsString();
+
+            System.out.println(nombre);
+
+            botones[i] = new JButton(nombre);
+            botones[i].setPreferredSize(new Dimension(146, botones[i].getPreferredSize().height));
+            botones[i].setMaximumSize(new Dimension(146, botones[i].getPreferredSize().height));
+            botones[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    mostrarPoke(url);
+                    mostrarInfo(url);
+                }
+            });
+
+            panelBotones.add(botones[i]);
+            System.out.println("Boton agregado: " + nombre);
+
+        }
+
+        panelBotones.revalidate();
+        panelBotones.repaint();
+        System.out.println(lista);
+
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        panelBotones.removeAll();
+       ConsumoAPI consumo = new ConsumoAPI();
+        String respuesta = consumo.consumoGET(anterior);
+        JsonObject list = JsonParser.parseString(respuesta).getAsJsonObject();
+        siguiente = list.get("next").getAsString();
+        if (list.has("previous") && !list.get("previous").isJsonNull()) {
+                anterior = list.get("previous").getAsString();
+            } else {
+                anterior = null;
+            }
+        System.out.println(siguiente);
+        
+        lista = list.getAsJsonArray("results");
+        System.out.println(list);
+
+        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
+        botones = new JButton[lista.size()];
+        for (int i = 0; i < lista.size(); i++) {
+            JsonObject pokemon = lista.get(i).getAsJsonObject();
+            String nombre = pokemon.get("name").getAsString();
+            String url = pokemon.get("url").getAsString();
+
+            System.out.println(nombre);
+
+            botones[i] = new JButton(nombre);
+            botones[i].setPreferredSize(new Dimension(146, botones[i].getPreferredSize().height));
+            botones[i].setMaximumSize(new Dimension(146, botones[i].getPreferredSize().height));
+            botones[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    mostrarPoke(url);
+                    mostrarInfo(url);
+                }
+            });
+
+            panelBotones.add(botones[i]);
+            System.out.println("Boton agregado: " + nombre);
+
+        }
+
+        panelBotones.revalidate();
+        panelBotones.repaint();
+        System.out.println(lista);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel icono;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPanel panelBotones;
